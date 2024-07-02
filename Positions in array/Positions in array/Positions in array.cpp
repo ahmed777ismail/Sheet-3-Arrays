b90c991dde5f8ed335aa95f3f71d2411596b2ad6
ahmed ismail <87954455+ahmed777ismail@users.noreply.github.com>
2024-07-03 01:06:47 +0300

    Add files via upload
    Positions in array

---
+++ Positions in array/Positions in array/Positions in array.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+	int n;
+	cin >> n;
+	int* arr = new int[n];
+	for (int i = 0; i < n; i++)
+	{
+		cin >> arr[i];
+		if (arr[i] <= 10)
+		{
+			cout << "A[" << i << "] = " << arr[i] << endl;
+		}
+	}
+
+}
+
