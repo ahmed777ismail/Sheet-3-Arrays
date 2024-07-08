3b801c0fb0896814d64108cecd7fd89a72a481a8
ahmed ismail <87954455+ahmed777ismail@users.noreply.github.com>
2024-07-08 11:41:58 +0300

    Add files via upload
    Smallest Pair

---
+++ Smallest Pair/Smallest Pair/Smallest Pair.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+	int t;
+	cin >> t;
+	while (t--)
+	{
+		int n;
+		cin >> n;
+		int* arr = new int[n + 1];
+		for (int i = 1; i <= n; i++)
+		
+			cin >> arr[i];
+			int mini = INT_MAX;
+		
+		for (int i = 1; i <= n; i++)
+		{
+			for (int j = i + 1; j <= n; j++) 
+			{
+				mini = min(mini, (arr[i] + arr[j] + j - i));
+			}
+		}
+		cout << mini << endl;
+	}
+	return 0;
+}
+
