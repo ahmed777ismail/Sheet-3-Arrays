35db6875579b454451e91299e6578a558c7e1faa
ahmed ismail <87954455+ahmed777ismail@users.noreply.github.com>
2024-07-10 12:39:02 +0300

    Add files via upload
    Sum Digits

---
+++ Sum Digits/Sum Digits.cpp
@@ -0,0 +1,20 @@
+using namespace std;
+#include <iostream>
+
+int main()
+{
+    int n;
+    cin >> n;
+    string s;
+    cin >> s;
+    int sum = 0;
+
+    for (int i = 0; i < n; i++)
+    {
+        sum += (int)(s[i] - '0');
+    }
+    cout << sum << endl;
+    return 0;
+
+}
+
